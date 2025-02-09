a388dfc15a69d601bcf0a54924495ee47ed0025e
lero2301 <lero2301@student.miun.se>
2025-02-09 19:37:43 +0100

    initial commit

---
+++ src/main/java/se/miun/dt133g/zkgithelper/App.java
@@ -0,0 +1,10 @@
+package se.miun.dt133g.zkgithelper;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
